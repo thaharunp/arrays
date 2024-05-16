df9e4b0c4a53bf63a49c15087ae2b8d8d0534e65
sadhakunps <sadaqunps@gmail.com>
2024-05-16 19:04:18 +0530

    array program

---
+++ TestOne.java
@@ -0,0 +1,17 @@
+class Test{
+    public static void main(String []t){
+
+        // Array declaration statement
+        int[] eids;
+        eids = new int[4];
+        eids[0]=101;
+        eids[1]=102;
+        eids[2]=103;
+        eids[3]=104;
+
+        System.out.println(eids[0]); 
+
+
+        System.out.println(eids); //Address  - [I@4517d9a3
+    }
+}
---
+++ TestTwo.java
@@ -0,0 +1,12 @@
+class Test{
+    public static void main(String[]t){
+       int   eids[]={22,6,9};
+       String ename[]={"thaharun","thaiba","tharun"};
+
+       System.out.println(eids[2]); //9
+       System.out.println(ename[2]); //tharun
+
+       System.out.println(ename); // int address
+       System.out.println(eids);   // string address
+    }
+}
