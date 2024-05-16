77956e800b1831b4477243c978b0124b70b3e46e
sadhakunps <sadaqunps@gmail.com>
2024-05-16 19:30:56 +0530

    r

---
+++ TestFour.java
@@ -0,0 +1,19 @@
+class Test{
+    public static void main(String[]u){
+
+        // Array declaration, creation, initilization separetly
+        String enames[];
+        enames = new String[4];
+        enames[0]="thaharun";
+        enames[1]= "thaiba";
+
+        // read array element
+        System.out.println(enames[0]);
+        System.out.println(enames[1]);
+        System.out.println(enames[2]); //null
+        System.out.println(enames[3]); //null
+ 
+ 
+
+    }
+}
--- TestThree.java
+++ TestThree.java
@@ -3,12 +3,10 @@ class Test{
         boolean [] discount;
         discount = new boolean[3];
 
-       /* System.out.println(discount[0]);
+        System.out.println(discount[0]);
         System.out.println(discount[1]);
-        System.out.println(discount[2]); */
+        System.out.println(discount[2]); 
 
-        discount [0]=true;
-        discount [1]=true;
-        discount [2]=true;
+        
     }
 }
