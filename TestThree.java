8247fcacb23e84f97903d91a1d4ddf766ee31673
sadhakunps <sadaqunps@gmail.com>
2024-05-16 19:14:05 +0530

    t

--- TestThree.java
+++ TestThree.java
@@ -4,6 +4,7 @@ class Test{
         discount = new boolean[3];
 
         System.out.println(discount[0]);
-        System.out.println(discount[0]);
+        System.out.println(discount[1]);
+        System.out.println(discount[2]);
     }
 }
